e23a232cbbc82dde1a7517cc53fabe60668cf411
Maycon Manini <maycon.manini@yahoo.com>
2024-09-11 21:12:50 -0300

    First Time with C#
    Não é tão intuitivo

---
+++ Aprendendo_CSharp_001/ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿using System;
+    Console.WriteLine("Achei meio estranho a forma de criar isso no vscode!");
+    Console.WriteLine("Mas acontece, tava acostumado com coisas que só de criar o código já rodava, era só criar o texto.alguma coisa e rodar");
